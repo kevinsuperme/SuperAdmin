269849cd2bdd0abdf20cbc5b781fa7ab36970cd1
妙码生花 <18523774412@qq.com>
2025-04-20 11:47:34 +0800

    refactor:跨域 methods 和 headers 默认允许所有

--- app/common/middleware/AllowCrossDomain.php
+++ app/common/middleware/AllowCrossDomain.php
@@ -26,8 +26,8 @@ class AllowCrossDomain
     protected array $header = [
         'Access-Control-Allow-Credentials' => 'true',
         'Access-Control-Max-Age'           => 1800,
-        'Access-Control-Allow-Methods'     => 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers'     => 'think-lang, server, ba_user_token, ba-user-token, ba_token, ba-token, batoken, Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-CSRF-TOKEN, X-Requested-With',
+        'Access-Control-Allow-Methods'     => '*',
+        'Access-Control-Allow-Headers'     => '*',
     ];
 
     /**
