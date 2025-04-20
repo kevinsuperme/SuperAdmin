289061905a0adce756272d2998d02fa161d60818
妙码生花 <18523774412@qq.com>
2025-04-20 20:09:33 +0800

    fix:修复 createAxios 取消重复请求的配置项大小写错误

--- web/src/utils/axios.ts
+++ web/src/utils/axios.ts
@@ -63,7 +63,7 @@ function createAxios<Data = any, T = ApiPromise<Data>>(axiosConfig: AxiosRequest
     // 合并默认请求选项
     options = Object.assign(
         {
-            CancelDuplicateRequest: true, // 是否开启取消重复请求, 默认为 true
+            cancelDuplicateRequest: true, // 是否开启取消重复请求, 默认为 true
             loading: false, // 是否开启loading层效果, 默认为false
             reductDataFormat: true, // 是否开启简洁的数据结构响应, 默认为true
             showErrorMessage: true, // 是否开启接口错误信息展示,默认为true
@@ -78,7 +78,7 @@ function createAxios<Data = any, T = ApiPromise<Data>>(axiosConfig: AxiosRequest
     Axios.interceptors.request.use(
         (config) => {
             removePending(config)
-            options.CancelDuplicateRequest && addPending(config)
+            options.cancelDuplicateRequest && addPending(config)
             // 创建loading实例
             if (options.loading) {
                 loadingInstance.count++
@@ -360,7 +360,7 @@ interface LoadingInstance {
 }
 interface Options {
     // 是否开启取消重复请求, 默认为 true
-    CancelDuplicateRequest?: boolean
+    cancelDuplicateRequest?: boolean
     // 是否开启loading层效果, 默认为false
     loading?: boolean
     // 是否开启简洁的数据结构响应, 默认为true
