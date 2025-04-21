d0b3561308415cbefdade277e661adf1ef032650
妙码生花 <18523774412@qq.com>
2025-04-21 21:18:59 +0800

    refactor(CRUD):历史记录表增加 comment 和 sync 字段

--- database/migrations/20250412134127_version222.php
+++ database/migrations/20250412134127_version222.php
@@ -1,6 +1,7 @@
 <?php
 
 use think\facade\Db;
+use app\admin\model\CrudLog;
 use think\migration\Migrator;
 
 class Version222 extends Migrator
@@ -43,16 +44,37 @@ class Version222 extends Migrator
         Db::name('admin')->where('status', '1')->update(['status' => 'enable']);
 
         /**
+         * CRUD 历史记录表
+         */
+        $crudLog = $this->table('crud_log');
+        if (!$crudLog->hasColumn('comment')) {
+            $crudLog
+                ->addColumn('comment', 'string', ['limit' => 255, 'default' => '', 'comment' => '注释', 'null' => false, 'after' => 'table_name'])
+                ->addColumn('sync', 'boolean', ['default' => 0, 'signed' => false, 'comment' => '已同步:0=否,1=是', 'null' => false, 'after' => 'fields'])
+                ->save();
+
+            $logs = CrudLog::select();
+            foreach ($logs as $log) {
+                if ($log->table['comment']) {
+                    $log->comment = $log->table['comment'];
+                    $log->save();
+                }
+            }
+        }
+
+        /**
          * 多个数据表的 status 字段类型修改为更合理的类型
          */
         $tables = ['admin_group', 'admin_rule', 'user_group', 'user_rule', 'security_data_recycle', 'security_sensitive_data', 'test_build'];
         foreach ($tables as $table) {
-            $mTable = $this->table($table);
-            $mTable->changeColumn('status', 'boolean', ['default' => 1, 'signed' => false, 'comment' => '状态:0=禁用,1=启用', 'null' => false])->save();
+            if ($this->hasTable($table)) {
+                $mTable = $this->table($table);
+                $mTable->changeColumn('status', 'boolean', ['default' => 1, 'signed' => false, 'comment' => '状态:0=禁用,1=启用', 'null' => false])->save();
 
-            // 原状态值兼容至新类型
-            Db::name($table)->where('status', 1)->update(['status' => 0]);
-            Db::name($table)->where('status', 2)->update(['status' => 1]);
+                // 原状态值兼容至新类型
+                Db::name($table)->where('status', 1)->update(['status' => 0]);
+                Db::name($table)->where('status', 2)->update(['status' => 1]);
+            }
         }
     }
 }
